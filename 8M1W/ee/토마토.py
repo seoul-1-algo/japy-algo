d3983f941a5eac064b86f50fd6ee477df3dbb779
ebeleey <ebeleey@gmail.com>
2025-08-04 17:51:32 +0900

    8M1W/ee: boj 7569 토마토 (py 주석추가)

--- 8M1W/ee/토마토.py
+++ 8M1W/ee/토마토.py
@@ -4,12 +4,7 @@ sys.stdin = open('7569.txt')
 from collections import deque
 
 M, N, H = map(int, input().split()) 
-
 tomatoes = [[list(map(int, input().split())) for _ in range(N)] for __ in range(H)]
-
-di = [1, -1, 0, 0, 0, 0]
-dj = [0, 0, 1, -1, 0, 0]
-dk = [0, 0, 0, 0, 1, -1]
 
 days = 0 # 모든 토마토가 익을 때 까지 며칠이 걸리는 지
 cnt = 0 # 익은 토마토의 수
@@ -27,21 +22,32 @@ for i in range(H):
 
             cnt += 1 # 익은 토마토와 비어있는 칸 세기
 
-if cnt == M * N * H: # 이미 상자안의 토마토가 모두 익어있음
+# 이미 상자안의 토마토가 모두 익어있으면 탈출
+if cnt == M * N * H: 
     print(0)
     exit()
     
+# BFS로 토마토 익히기 !!!!
+
+# 영향을 줄 수 있는 방향
+di = [1, -1, 0, 0, 0, 0]
+dj = [0, 0, 1, -1, 0, 0]
+dk = [0, 0, 0, 0, 1, -1]
+
 while q:
     i, j, k = q.popleft()
-    for l in range(6):
+    
+    for l in range(6): # 6방향에 대해서
         ni, nj, nk = i + di[l], j + dj[l], k + dk[l]
+        
         if 0 <= ni < H and 0 <= nj < N and 0 <= nk < M and tomatoes[ni][nj][nk] == 0: # 토마토가 익지 않았다면
-            tomatoes[ni][nj][nk] = tomatoes[i][j][k] + 1
-            cnt += 1
-            q.append((ni, nj, nk))
-            days = max(days, tomatoes[ni][nj][nk])
+            tomatoes[ni][nj][nk] = tomatoes[i][j][k] + 1 # 영향을 주는 토마토의 day보다 +1
+            q.append((ni, nj, nk)) # 큐에 추가
+            
+            cnt += 1 # 익은 토마토의 개수 +1
+            days = max(days, tomatoes[ni][nj][nk]) # 걸리는 days 업데이트
 
-if cnt < M * N * H: #모든 토마토가 익지 못한다면
+if cnt < M * N * H: # 모든 토마토가 익지 못한다면
     print(-1)
-else:
+else: # 걸리는 일수 출력
     print(days - 1)
