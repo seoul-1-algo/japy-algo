1be8837f144cc06e63230e19e3e42eefdd0f6190
ebeleey <ebeleey@gmail.com>
2025-10-01 22:40:11 +0900

    10M1W/ee: 백준 2467 용액(py)

---
+++ 10M1W/ee/용액.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open('2467.txt')
+
+N = int(input()) 
+liquids = list(map(int, input().split())) # 용액
+
+front = 0
+rear = N-1
+
+min_val = 1e9
+ans = [front, rear]
+
+while front < rear:
+    temp = liquids[front] + liquids[rear]
+
+    if min_val >= abs(temp):
+        min_val = abs(temp)
+        ans = [front, rear]
+
+    if temp > 0:
+        rear -= 1
+    elif temp < 0:
+        front += 1
+    else:
+        break
+
+print(liquids[ans[0]], liquids[ans[1]])
